56993f39bd3118a53ef2435d7c31a00542443a20
BoatBambe <120298072+Boatbambe@users.noreply.github.com>
2023-09-19 17:18:17 +0700

    Tipe Data Boolean

---
+++ boolean.php
@@ -0,0 +1,16 @@
+<?php 
+// tipe data Boolean ada true dan false
+
+$x = true; //true sama dengan 1
+$x = false; // False sama dengan 0
+
+// operasional ada && dan ||
+// AND && sama dengan x
+$and = $x && $y && true;
+
+// var_dump($and)
+// or= +
+$or = $x || $y;
+
+var_dump ($or)
+?>
